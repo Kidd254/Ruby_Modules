906eb1a0e86b39901d13690383a1066a01de2a7f
Javier Grau <javiergrau.freelance@gmail.com>
2023-08-25 14:04:45 -0400

    Fix bug & add linters

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,17 +1,17 @@
 module MyEnumerable
-    def all?(&block)
-        each {|e| return false unless block.call(e)}
-        true
-    end
-    
-    def any?(&block)
-        each {|e| return true if block.call(e)}
-        false
-    end
+  def all?(&block)
+    each { |e| return false unless block.call(e) }
+    true
+  end
 
-    def filter?(&block)
-        filtered = []
-        each {|e| filtered << if block.call(e)}
-        result
-    end
+  def any?(&block)
+    each { |e| return true if block.call(e) }
+    false
+  end
+
+  def filter(&block)
+    filtered = []
+    each { |e| filtered << e if block.call(e) }
+    filtered
+  end
 end
--- my_list.rb
+++ my_list.rb
@@ -1,15 +1,28 @@
 require_relative 'my_enumerable'
 
 class MyList
-    include MyEnumerable
-    def initialize(*elements)
-        @list = elements
-    end
+  include MyEnumerable
+  def initialize(*elements)
+    @list = elements
+  end
 
-    def each(&block)
-        @list.each {&block}
-    end
+  def each(&block)
+    @list.each(&block)
+  end
 end
 
-list = MyList.new(1, 2, 3, 4)
-puts (list.all? {|e| e < 5})
+# rubocop:disable all
+# Create our list
+puts list = MyList.new(1, 2, 3, 4) # => <MyList: @list=[1, 2, 3, 4]>
+
+# Test #all?
+puts(list.all? { |e| e < 5 }) # => true
+puts(list.all? { |e| e > 5 }) # => false
+
+# Test #any?
+puts(list.any? { |e| e == 2 }) # => true
+puts(list.any? { |e| e == 5 }) # => false
+
+# Test #filter
+print(list.filter { |e| e.even? }) # => [2, 4]
+# rubocop:enable all
